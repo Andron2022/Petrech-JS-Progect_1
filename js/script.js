c854b45243c0b1f2e057b1667ab954b3b8929607
AndChtk <ashevcab@gmail.com>
2025-02-12 16:32:28 +0500

    workHome2 & 3 done!

--- js/script.js
+++ js/script.js
@@ -21,9 +21,50 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+/* Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
+// Часть 3
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+  // while (numberOfFilms == '' || numberOfFilms == null || numberOfFilms == isNaN(numberOfFilms)) Ниже условие то же. Пока numberOfFilms не правда, а '' Null и Nan это ложь
+  while (!numberOfFilms) {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+  }
+}
+
+start();
+
 const personalMoveDB = {
   count: numberOfFilms,
   movies: {},
@@ -31,11 +72,52 @@ const personalMoveDB = {
   genres: [],
   privat: false,
 };
-// Создаем тех переменные для одноразового использования
-const a1 = prompt("Назовите один из последних просмотренных фильмов", ""),
-  b1 = prompt("На сколько вы его оцеиваете?", ""),
-  a2 = prompt("Назовите один из последних просмотренных фильмов", ""),
-  b2 = prompt("На сколько вы его оцеиваете?", "");
 
-personalMoveDB.movies[a1] = b1;
-personalMoveDB.movies[a2] = b2;
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt("Назовите один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько вы его оцеиваете?", "");
+
+    if (a && b && a.length < 50) {
+      personalMoveDB.movies[a] = b;
+      console.log("OK");
+    } else {
+      console.log("Err");
+      i--;
+    }
+  }
+}
+
+//rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMoveDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+  } else if (personalMoveDB.count >= 10 && personalMoveDB.count <= 30) {
+    console.log("Вы классический зритель");
+  } else if (personalMoveDB.count > 30) {
+    console.log("Вы киноман");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+
+//detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) console.log(personalMoveDB);
+}
+
+showMyDB(personalMoveDB.privat);
+//console.log(personalMoveDB);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMoveDB.genres[i - 1] = prompt(
+      `Ваш любимый жанр под номером ${i}?`,
+      ""
+    );
+  }
+}
+
+writeYourGenres();
